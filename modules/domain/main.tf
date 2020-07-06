5f838ad028e4a1a2ae05643ca52067766c1197eb
Chris Nesbitt-Smith <chris@cns.me.uk>
2020-07-06 17:15:49 +0100

    modules

---
+++ modules/domain/main.tf
@@ -0,0 +1,49 @@
+variable "domain" {
+  description = "domain name"
+}
+
+variable "redirect_nowww" {
+  description = "redirect non www to www."
+  default = false
+}
+
+variable "s3" {
+  description = "host www on s3"
+  default = false
+}
+
+resource "cloudflare_zone" "zone" {
+  zone = var.domain
+  plan = "free"
+}
+
+resource "cloudflare_record" "www" {
+  count = var.s3 ? 1 : 0
+  zone_id = cloudflare_zone.zone.id
+  name    = "www"
+  value   = "www.${var.domain}.s3-website.eu-west-2.amazonaws.com"
+  type    = "CNAME"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "A" {
+  count = var.redirect_nowww ? 1 : 0
+  zone_id = cloudflare_zone.zone[count.index].id
+  name    = "@"
+  value   = "45.55.72.95"
+  type    = "A"
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "txt" {
+  count = var.redirect_nowww ? 1 : 0
+  zone_id = cloudflare_zone.zone[count.index].id
+  name    = "_redirect"
+  value   = "Redirects from /* to http://www.${var.domain}/*"
+  type    = "TXT"
+  ttl     = 3600
+}
+
+output "zone" {
+  value = cloudflare_zone.zone
+}
